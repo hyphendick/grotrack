72f9a1aa4347f2251753895a7650120bef5f98d5
D. Love <d.d.love20@gmail.com>
2013-04-27 23:56:25 -0700

    test data to bootstrap.grovy

--- grails/grotrack/grails-app/conf/BootStrap.groovy
+++ grails/grotrack/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,8 @@
 class BootStrap {
 
     def init = { servletContext ->
+    	def workout1 = new Workout(name: 'workout 1').save()
+        def workout2 = new Workout(name: 'Workout 2').save()
     }
     def destroy = {
     }
